6d482e045ca09edf05d38a38122e9e5311dfef04
zulianc <julien.chataigner@etu.unic-lyon1.fr>
2025-01-20 11:23:06 +0100

    ajout tableau morts par departements

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $a = array(array("nom" => "rhone", "nombre" => 69), array("nom" => "loire", "nombre" => 42)); // pour l'exemple, il faudra interroger la base
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'departements' => $a,
         ]);
     }
 }
