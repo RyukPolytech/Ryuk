b9c7536e8ec41bb6084e4646c8f96e32352545fd
Melvyn <melvyn.bauvent@etu.univ-lyon1.fr>
2025-01-22 12:34:01 +0100

    hotfix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -26,8 +26,6 @@ class HomeController extends AbstractController
             //Ayaya
         }
 
-        echo $departmentRepository->count([]) ;
-
         $a = array(array("nom" => "rhone", "nombre" => 69), array("nom" => "loire", "nombre" => 42)); // pour l'exemple, il faudra interroger la base
         $b = array(array("prenom" => "julien", "nombre" => 120), array("prenom" => "lilas", "nombre" => 240), array("prenom" => "merlvyn", "nombre" => 60)); // pour l'exemple, il faudra interroger la base
         return $this->render('home/index.html.twig', [
