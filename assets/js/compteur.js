1b75100a0909c931f2df42478b4e4929f5dab3fc
zulianc <julien.chataigner@etu.univ-lyon1.fr>
2025-01-22 11:53:42 +0100

    compteur de morts

---
+++ assets/js/compteur.js
@@ -0,0 +1,19 @@
+const compteur = document.getElementById("compteur");
+setInterval(() => {
+    let dontStop = true;
+    let i = 0;
+    while(dontStop) {
+        let incrementedCompteur = compteur.children.namedItem("compteur-num" + i);
+        if (incrementedCompteur.innerHTML == 9) {
+            incrementedCompteur.innerHTML = 0;
+            i++;
+        }
+        else {
+            incrementedCompteur.innerHTML = Number(incrementedCompteur.innerHTML) + 1;
+            dontStop = false;
+        }
+        if (i >=  10) {
+            dontStop = false;
+        }
+    }
+}, 555)
