79bde6da3e788163bdb8d889aa0344be16876331
Janet-Doe <lilas.grenier@etu.univ-lyon1.fr>
2025-01-22 18:31:36 +0100

    Add prediction form and enable CSRF protection; update index templates and styles

--- src/Controller/AssuranceController.php
+++ src/Controller/AssuranceController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\PredictionType;
 
 final class AssuranceController extends AbstractController
 {
     #[Route('/assurance', name: 'app_assurance')]
     public function index(): Response
     {
+        $form = $this->createForm(PredictionType::class);
         return $this->render('assurance/index.html.twig', [
             'controller_name' => 'AssuranceController',
+            'form' => $form,
         ]);
     }
 }
