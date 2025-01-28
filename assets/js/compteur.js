dcdbc37d26a20cf38b07323cb2fdb58db632ac16
zulianc <julien.chataigner@etu.univ-lyon1.fr>
2025-01-28 15:10:13 +0100

    fix css ptet

--- assets/js/compteur.js
+++ assets/js/compteur.js
@@ -5,6 +5,11 @@ setInterval(() => {
     while(dontStop) {
         let incrementedCompteur = compteur.children.namedItem("compteur-num" + i);
         if (incrementedCompteur.innerHTML == 9) {
+            if (i === 1) {
+                const audio = document.getElementById("death");
+                audio.playbackRate=0.5;
+	            audio.play();
+            }
             incrementedCompteur.innerHTML = 0;
             i++;
         }
